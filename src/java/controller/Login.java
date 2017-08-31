f2e1a633c54b57e745f607ce4a98220698aa0e8f
roja jayashree <roja4jayashree@gmail.com>
2017-08-31 08:03:47 -0400

    db connection success and user  login success

---
+++ src/java/controller/Login.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+import DBUtils.DBConnection;
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author rkarne
+ */
+
+@Named//correct one from java application 7 if u want to use java 6 then use facelets.*
+@SessionScoped
+
+public class Login implements Serializable{
+    private String user;
+    private int pass;
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPass(int pass) {
+        this.pass = pass;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public int getPass() {
+        return pass;
+    }
+    
+    public Login(){
+        
+    }
+    
+    public void dologin(){
+        //if(user.equals("roja") && 123 == pass){
+            //return username + " , " + password; 
+            //return "loginsuccess";
+        //}
+        //else{
+          //  return "Please try again";
+        //
+        try {
+            Connection con = DBConnection.getConnection();
+            Statement stm = con.createStatement();
+            ResultSet rs = stm.executeQuery("SELECT * FROM users");
+             while (rs.next()) {
+                 user = rs.getString(1);
+                 pass = rs.getInt(2);
+                 if(pass == rs.getInt(2)){
+                     //return 'loginsuccess';
+                    System.out.println(pass);
+                    System.out.println(user);
+                 }
+                
+             }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //return null;
+    }
+}
